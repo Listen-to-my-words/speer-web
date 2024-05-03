a426a3481b84db666417def8dabf1549151ad616
SaltySalt77 <alexiena@naver.com>
2024-05-04 01:33:00 +0900

    Chore: Removed testcodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css' // eslint-disable-next-line
 import { Route, Routes } from 'react-router-dom'
-import Hello from './components/Hello'
 import Root from './pages/Root'
-import SignIn from './pages/SignIn'
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<Root />} />
-      <Route path={'/hello'} element={<Hello />} />
-      <Route path={'/signIn'} element={<SignIn />} />
       <Route path={'*'} element={<h1>404</h1>} />
     </Routes>
   )
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Nav from '../components/Nav'
 
 const Root = () => {
-  return <Nav />
+  return <>root</>
 }
 
 export default Root
