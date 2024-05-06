f257cad358bf2dfb3fd25a6db130b2c7a4841424
SaltySalt77 <alexiena@naver.com>
2024-05-06 23:55:08 +0900

    Feat: Add navigate to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,18 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Logo = () => {
-  return <Box component={'img'} src={'https://picsum.photos/90'} width={'8rem'} height={'8rem'} />
+  const navigate = useNavigate()
+  return (
+    <Box
+      component={'img'}
+      src={'https://picsum.photos/90'}
+      width={'8rem'}
+      height={'8rem'}
+      onClick={() => navigate('/')}
+    />
+  )
 }
 
 export default Logo
